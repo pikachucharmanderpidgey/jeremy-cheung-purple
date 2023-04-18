ffb30554d583c489dcf3dc89902e033f2f4842bd
emmahuwu <emma.han.534@gmail.com>
2023-04-18 19:01:48 -0400

    act 4

---
+++ JC-Sumerian Game1/Assets/Player.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Player : MonoBehaviour
+{
+    public string playerName;
+    private ulong highestPop;
+    private GameplayManager gameManager;
+
+    public TMP_Text nameText;
+    public TMP_Text yearText;
+    public TMP_Text highestPopText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nameText.text = "Ruler:" + playerName;
+
+        gameManager = GetComponent<GameplayManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yearText.text = "Year:" + gameManager.year;
+        if(gameManager.population > highestPop)
+{
+            highestPop = gameManager.population;
+            highestPopText.text = "Highest Population:" + highestPop;
+        }
+    }
+}
+
+   
