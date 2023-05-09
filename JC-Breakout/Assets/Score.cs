04ac3c9526ae7f90962918192ff9640ae895c767
Miles <milestonesxh@gmail.com>
2023-05-09 18:01:46 -0400

    Done
    Done

--- JC-Breakout/Assets/Score.cs
+++ JC-Breakout/Assets/Score.cs
@@ -5,16 +5,24 @@ using TMPro;
 
 public class Score : MonoBehaviour
 {
-    public TMP_Text score_Text scoreText;
+    public TMP_Text scoreText;
     private int score;
     // Start is called before the first frame update
     void Start()
     {
-
+        score = 0;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(collision.gameObject);
+        if (collision.gameObject.CompareTag("Brick"))
+        {
+            Destroy(collision.gameObject);
+            score++;
+            scoreText.text = score.ToString();
+        }
+
     }
 }
+  
+
